abf100d893755719a7818c137a8369d8b1beced1
francesca2 <francesca.giordani88@gmail.com>
2016-11-03 15:06:26 +0100

    Secondo esempio

---
+++ ObjectInjection/src/main/java/it/alfasoft/francesca/ObjectInjection/Esempio.java
@@ -0,0 +1,19 @@
+package it.alfasoft.francesca.ObjectInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Esempio {
+
+	public static void main(String[] args) {
+		
+	
+		 ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		 
+		   Triangolo t=(Triangolo) context.getBean("triangolo");
+		   
+		   t.stampaVertici();
+		   
+	}
+
+}
