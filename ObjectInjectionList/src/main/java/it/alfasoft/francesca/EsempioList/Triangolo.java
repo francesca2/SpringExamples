c8846ba77d5ed6958cbf67621de8d99e40906483
francesca2 <francesca.giordani88@gmail.com>
2016-11-03 15:21:05 +0100

    esempio liste

---
+++ ObjectInjectionList/src/main/java/it/alfasoft/francesca/EsempioList/Triangolo.java
@@ -0,0 +1,33 @@
+package it.alfasoft.francesca.EsempioList;
+
+import java.util.List;
+
+public class Triangolo {
+	
+	  private List<Punto> punti;
+	
+	  public Triangolo() {
+			
+		}
+
+	public void stampaVertici(){
+		
+		for(Punto p : punti){
+		System.out.println(p.getX()+" "+p.getY());
+		}
+	
+	}
+
+
+
+	public List<Punto> getPunti() {
+		return punti;
+	}
+
+
+
+	public void setPunti(List<Punto> punti) {
+		this.punti = punti;
+	}
+
+}
